d6372af89ca53a1b98cdb4aeac5f69264e42b412
WoZhi <776897732@qq.com>
2022-08-04 21:58:42 +0800

    使用Firebase实现谷歌账号的登录以及用户数据存储FireStore

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,17 @@
+import {
+  signInWithGooglePopup,
+  createUserDocmentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocmentFromAuth(user);
+  };
   return (
     <div>
       <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in withGoogle Popup</button>
     </div>
   );
 };
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,53 @@
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBk2jI0TcwC-S5K_1dI29SFSRSfwGW-wSI",
+  authDomain: "crwn-clothing-db-bb033.firebaseapp.com",
+  projectId: "crwn-clothing-db-bb033",
+  storageBucket: "crwn-clothing-db-bb033.appspot.com",
+  messagingSenderId: "271062248998",
+  appId: "1:271062248998:web:2170fca6815146ebc380e7",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocmentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  //   console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  //   console.log(userSnapshot);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("Error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
